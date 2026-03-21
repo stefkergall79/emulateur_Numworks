966035910af5c2d86918b51a570d4c247c07c7fa
E256885Y <stef.kergall@gmail.com>
2026-03-21 18:54:49 +0100

    Refactor set_pixel and fill_rect functions to use 'col' parameter; add docstrings for clarity

--- kandinsky.py
+++ kandinsky.py
@@ -19,29 +19,39 @@ def __check_int_type(args):
         if not isinstance(i, int):
             raise TypeError(f"can't convert {type(i).__name__} to int")
 
-
-def set_pixel(x, y, color):
+def set_pixel(x, y, col):
+    """
+    Colore le pixel (x,y)
+    """
     __check_int_type((x, y))
-    update(rect(__screen, color,
+    update(rect(__screen, col,
                 (x * __Ratio_ecran, y * __Ratio_ecran, __Ratio_ecran, __Ratio_ecran)))
 
 
 def get_pixel(x, y):
+    """
+    Renvoie la couleur du pixel (x,y)
+    """
     __check_int_type((x,y))
     return __screen.get_at((x * __Ratio_ecran, y * __Ratio_ecran))[:3]
 
 
-def fill_rect(x, y, w, h, color):
+def fill_rect(x, y, w, h, col):
+    """
+    Remplit un rectangle
+    """
     __check_int_type((x, y, w, h))
-    update(rect(__screen, color, (x * __Ratio_ecran, y * __Ratio_ecran,
+    update(rect(__screen, col, (x * __Ratio_ecran, y * __Ratio_ecran,
                                 w * __Ratio_ecran, h * __Ratio_ecran)))
 
 __fonte_kandinsky = Font("ter-u18b.bdf")
 def draw_string(text, x, y, color = "black", background = "white"):
+    """
+    Affiche un texte au pixel (x,y)
+    """
     __check_int_type((x,y))
     if not isinstance(text, str):
-        raise TypeError(f"can't convert {type(text).__name__} to str implicitly")
-    
+        raise TypeError(f"can't convert '{type(text).__name__}' object to str implicitly")
     __screen.blit(scale(__fonte_kandinsky.render(text, False, color, background),
                         (__Ratio_ecran * 10 * (len(text)-text.count("\u0301")),
                          18 * __Ratio_ecran)),
@@ -49,4 +59,7 @@ def draw_string(text, x, y, color = "black", background = "white"):
     flip()
 
 def color(r, g, b):
+    """
+    Définit une couleur rvb
+    """
     return (r, g, b)
